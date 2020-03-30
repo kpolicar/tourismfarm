d5a46181c5fe36a45490a7773f7e1d6e1bb594f3
Klemen <naltamer14@gmail.com>
2020-03-30 14:04:03 +0200

    create reservation migration, model, controller, factory

---
+++ database/migrations/2020_03_30_110204_create_reservations_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->text('message')
+                ->nullable();
+            $table->date('from')
+                ->useCurrent();
+            $table->date('to');
+            $table->integer('adults')
+                ->unsigned()
+                ->default(1);
+            $table->integer('children')
+                ->unsigned()
+                ->default(0);
+            $table->integer('infants')
+                ->unsigned()
+                ->default(0);
+            $table->integer('price')
+                ->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
