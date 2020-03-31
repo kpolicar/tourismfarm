dd9c77624f8c4eacdf664ab14d5cbdd77c61a46f
Klemen <naltamer14@gmail.com>
2020-03-31 21:28:56 +0200

    add database url env

--- config/database.php
+++ config/database.php
@@ -57,6 +57,7 @@ return [
         ],
 
         'pgsql' => [
+            'url' => env('DATABASE_URL'),
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
